4572ab242d2803b0dec4059cc1971575d6ba30f7
quocngo-git <ngoquoc.work@gmail.com>
2020-10-06 20:36:32 -0700

    update Cat.java

--- lab7/Cat.java
+++ lab7/Cat.java
@@ -1,46 +1,81 @@
 import java.util.Scanner;
-public class Cat {
+public abstract class Cat {
     
+    /**
+     * Cat's name
+     */
     private String name;
+
+    /**
+     * Cat's hunger levels that determine its action
+     * Hunger levels are between 0-10
+     */
     private int hunger;
 
     /**
-     * Constructor Cat is created 
-     * @param n gets name of the cat
-     * @return
+     * Overloaded constructor Cat is created 
+     * @param n gets teh name of the cat
      */
-    publlic Cat(String n){
+    public Cat(String n){
         Scanner sc = new Scanner(System.in);
         System.out.print("Name your kitty: ");
         n = sc.nextLine();
         name = n;
+        hunger = 6;
     }
-
+    /**
+     * Get cat's name
+     * @return string cat's name for displaying
+     */
     public String getName(){
         return name;
     }
 
-    public getHunger(){
+    /**
+     * Cat hunger levels are used to determine cat's action
+     * @return cat's hunger levels
+     */
+    public int getHunger(){
         return hunger;
     }
-
-    protected incrementHunger(int val){
-
+    
+    /**
+     * Adjust level of hunger based on player's action
+     * @param val different actions leads to different values
+     * @return int current hunger
+     */
+    protected int incrementHunger(int val){
+        hunger += val;
+        if (hunger < 1) return 1;
+        else if (hunger > 10) return 10;
+        else return hunger;
     }
 
+    /**
+     * display cat's current state based on hunger levels
+     * return String representations of hunger levels
+     */
     public String toString(){
-
+        if(hunger > 0 && hunger <5) return name + " is hungry";
+        else if (hunger < 9)  return name + " is satisfied";
+        else return name + " is full";
     }
 
-    public abstract String feed(Player P){
+    /**
+     * Player feeds cat
+     * @param P the player
+     */
+    public abstract String feed(Player P);
 
-    }
+    /**
+     * Player plays with cat
+     * @param P the player
+     */
+    public abstract String play(Player p);
 
-    public abstract String play(Player p){
-
-    }
-
-    public abstract String pet(Player p){
-
-    }
+    /**
+     * Player pets cat
+     * @param P the player
+     */
+    public abstract String pet(Player p);
 }

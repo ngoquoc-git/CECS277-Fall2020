41192af24460f30f0aed8e29594e20ce0b91fb9e
quocngo-git <ngoquoc.work@gmail.com>
2020-10-12 22:30:41 -0700

    Create Item and Point

--- project 1/Item.java
+++ project 1/Item.java
@@ -1,3 +1,19 @@
 public class Item {
-    
+    private String name;
+
+    /**
+     * Pass item's name to the Sring name
+     * @param name Item's name
+     */
+    public Item(String name){
+        this.name = name;
+    }
+
+    /**
+     * return item's name for further use
+     * @return string name item's name
+     */
+    public String getName(){
+        return name;
+    }
 }
--- project 1/Point.java
+++ project 1/Point.java
@@ -1,3 +1,8 @@
 public class Point {
-    
+    public int x;
+    public int y;
+
+    public Point(int x, int y){
+        
+    }
 }

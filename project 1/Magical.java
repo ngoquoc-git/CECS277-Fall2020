0b1b67292f2ca0916d83b3f5c6ef4afd3e125d40
quocngo-git <ngoquoc.work@gmail.com>
2020-10-13 14:29:09 -0700

    create javadoc for Magical

--- project 1/Magical.java
+++ project 1/Magical.java
@@ -1,10 +1,24 @@
 public interface Magical {
     public static final String MAGIC_MENU = "1. Magical Missile.\n2. Fire Ball.\n3. Thunderclap.\n";
 
-    
+    /**
+     * Use magical missile on entity e
+     * @param e Enity that will be attacked
+     * @return
+     */
     public String magicalMissile(Entity e);
     
+    /**
+     * Use fire ball on entity e
+     * @param e Enity that will be attacked
+     * @return
+     */
     public String fireBall(Entity e);
 
+    /**
+     * Use thunder clap on entity e
+     * @param e Enity that will be attacked
+     * @return
+     */
     public String thunderClap(Entity e);
 }

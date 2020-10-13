c735df90323241b7d8ff3a06877f0852e6d26963
quocngo-git <ngoquoc.work@gmail.com>
2020-10-13 15:11:57 -0700

    working on Map
    working on Map
    create arraylist Items in Hero

--- project 1/Hero.java
+++ project 1/Hero.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
 public class Hero {
+    /** Create an array list to store items*/
+    private ArrayList<Item> Items;
+
     
 }
--- project 1/Map.java
+++ project 1/Map.java
@@ -1,3 +1,46 @@
 public class Map {
-    
+    /** Create an axb map */
+    private char[][] map;
+
+    private boolean revealed;
+
+    /**
+     * Default constructor create a 5x5 map
+     */
+    public Map(){
+        map = new char[5][5];
+        
+    }
+
+    /**
+     * 
+     * @param mapNum
+     */
+    public void loadMap(int mapNum){
+
+    }
+
+    /**
+     * retrive the character at a specific location
+     * @param p object's location x, y 
+     * @return loc character at location x, y
+     */
+    public char getCharAtLoc(Point p){
+        char loc = map[p.x][p.y];
+        return loc;
+    }
+
+    /**
+     * 
+     * @param p object's location x, y
+     */
+    public void displayMap(Point p){
+
+    }
+
+    Point findStart(){
+        return p;
+    }
+
+
 }

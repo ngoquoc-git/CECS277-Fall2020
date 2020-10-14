85eb1b96047bf333635a6b2648bbf05b45dbcb33
quocngo-git <ngoquoc.work@gmail.com>
2020-10-13 19:07:59 -0700

    Update BasicDoor

--- lab8/BasicDoor.java
+++ lab8/BasicDoor.java
@@ -1,26 +1,49 @@
 public class BasicDoor implements Door{
 
-    public String examine(){
+    private boolean push;
 
+    private boolean input;
+
+    public BasicDoor(){
+        push = true;
+        input = true;
+    }
+
+    public String examine(){
+        return "You reach a basic door\n";
     }
     
     public String menu(){
-
+        return "1.Push\n2. Pull";
     }
 
     public String unlock(int option){
-
+        String doorOpen;
+        if(option == 1) {
+            input = true;
+            push = true;
+            doorOpen = success();
+        }
+        else{
+            input = false;
+            push = false;
+            System.out.println("Nothing happened, door remains close. Try again!");
+            doorOpen = unlock(option);
+        }
+        return doorOpen;
     }
 
     public boolean open(){
-
+        //if(input) return true;
+        //return false;
+        return input ? true : false;
     }
 
     public String clue(){
-
+        return "You can either push it or pull it to open\n";
     }
     
     public String success(){
-
+        return "You've successfully open this door.";
     }
 }

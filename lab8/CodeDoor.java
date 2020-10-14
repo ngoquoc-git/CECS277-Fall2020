f68167ca87b3b17370c32774caf202a0e7bb1beb
quocngo-git <ngoquoc.work@gmail.com>
2020-10-14 00:02:45 -0700

    bug fixed

--- lab8/CodeDoor.java
+++ lab8/CodeDoor.java
@@ -1,26 +1,76 @@
-/**public class CodeDoor implements Door{
-    
-    public String examine(){
+import java.util.Arrays;
+import java.util.Random;
+
+public class CodeDoor implements Door {
+
+    private char[] keys;
+    private char[] code;
+
+    public CodeDoor() {
+
+        keys = new char[]{'X', 'X', 'X'};
+
+        String option = "XO";
+        Random ran = new Random();
+        char code1 = option.charAt(ran.nextInt(option.length()));
+        char code2 = option.charAt(ran.nextInt(option.length()));
+
+        code = new char[]{'O', code1, code2};
 
     }
-    
-    public String menu(){
 
+    @Override
+    public String examine() {
+        return "A door with a coded keypad with three characters. " +
+                "\nEach key toggles a value with X or O";
     }
 
-    public String unlock(int option){
-
+    @Override
+    public String menu() {
+        return "\n1. Press Key 1\n2. Press Key 2\n3. Press Key 3\n";
     }
 
-    public boolean open(){
+    @Override
+    public String unlock(int option) {
 
+        if (option == 1) {
+
+            if (keys[0] == 'X') keys[0] = 'O';
+            else if (keys[0] == 'O') keys[0] = 'X';
+
+        } else if (option == 2) {
+
+            if (keys[1] == 'X') keys[1] = 'O';
+            else if (keys[1] == 'O') keys[1] = 'X';
+
+        } else {
+            //TODO check for input
+            if (keys[2] == 'X') keys[2] = 'O';
+            else if (keys[2] == 'O') keys[2] = 'X';
+        }
+
+        return "You pressed key number " + option;
     }
 
-    public String clue(){
+    @Override
+    public boolean open() {
+        return Arrays.equals(keys, code);
+    }
 
+    @Override
+    public String clue() {
+
+        int counter = 0;
+
+        if (code[0] == keys[0]) counter++;
+        if (code[1] == keys[1]) counter++;
+        if (code[2] == keys[2]) counter++;
+
+        return "Number of correct items: " + counter;
     }
-    
-    public String success(){
-        
+
+    @Override
+    public String success() {
+        return "Congratulations, you cracked the code and opened the door";
     }
-}*/
+} 
--- lab8/Main.java
+++ lab8/Main.java
@@ -1,7 +1,33 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+        System.out.println("Welcome to the Escape Room.");
+        System.out.println("You must unlock 3 doors to escape...");
 
+        for (int i = 0; i < 3; i++) {
+
+            Door d;
+            int door = (int) (Math.random() * 5) + 1;
+
+            if (door == 1) {
+                d = new BasicDoor();
+                openDoor(d);
+            } else if (door == 2) {
+                d = new LockedDoor();
+                openDoor(d);
+            } else if (door == 3) {
+                d = new DeadboltDoor();
+                openDoor(d);
+            } else if (door == 4) {
+                d = new ComboDoor();
+                openDoor(d);
+            } else if (door == 5) {
+                d = new CodeDoor();
+                openDoor(d);
+            }
+        }
+
+        System.out.println("Congratulations! You escaped...this time.");
     }
 
     public static void openDoor(Door d){

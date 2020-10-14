2eadff3743dbe035028a4dc2fa2919ecdb8b27cb
quocngo-git <ngoquoc.work@gmail.com>
2020-10-13 18:45:22 -0700

    Create interface classes

--- lab8/CodeDoor.java
+++ lab8/CodeDoor.java
@@ -1,3 +1,26 @@
 public class CodeDoor implements Door{
     
+    public String examine(){
+
+    }
+    
+    public String menu(){
+
+    }
+
+    public String unlock(int option){
+
+    }
+
+    public boolean open(){
+
+    }
+
+    public String clue(){
+
+    }
+    
+    public String success(){
+        
+    }
 }

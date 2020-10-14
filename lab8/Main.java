d85754e648585a195f4b76da7fba0be0548052e8
quocngo-git <ngoquoc.work@gmail.com>
2020-10-13 22:33:55 -0700

    finish openDoor

--- lab8/Main.java
+++ lab8/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
 
     }
 
     public static void openDoor(Door d){
-        
+        Scanner sc = new Scanner(System.in);
+        int decision;
+
+        System.out.print(d.examine());
+        System.out.print(d.menu());
+        while (!d.open()){
+            decision = sc.nextInt();
+            d.unlock(decision);
+            if(!d.open()) System.out.print(d.clue());
+            else System.out.print(d.success());
+        }
     }
 }

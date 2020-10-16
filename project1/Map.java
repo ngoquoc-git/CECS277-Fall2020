925d543e697623c6a0d7da7b5eaeee6673f497d5
quocngo-git <ngoquoc.work@gmail.com>
2020-10-16 00:09:04 -0700

    Update Map
    Finish loadMap and displayMap

--- project1/Map.java
+++ project1/Map.java
@@ -1,8 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Map {
     /** Create an axb map */
     private char[][] map;
 
-    private boolean revealed;
+    private boolean[][] revealed;
 
     /**
      * Default constructor create a 5x5 map
@@ -17,7 +20,23 @@ public class Map {
      * @param mapNum
      */
     public void loadMap(int mapNum){
-
+        try {
+            String mapLoad = "Map" + mapNum + ".txt";
+            File mapReader = new File(mapLoad);
+            Scanner myMap = new Scanner(mapReader);
+            while (myMap.hasNextLine()) {
+              for(int i = 0; i < map.length; i++){
+                String line = myMap.nextLine().replace(" ", "");
+                for (int j = 0; j < map.length; j++){
+                    map[i][j] = line.charAt(j);
+                    revealed[i][j] = false;
+                }
+              }
+            }
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
     }
 
     /**
@@ -31,16 +50,23 @@ public class Map {
     }
 
     /**
-     * 
+     * Display the 5x5 map with given revealed positions
      * @param p object's location x, y
      */
     public void displayMap(Point p){
-        
+        for (int i = 0; i < map.length; i++){
+            for (int j = 0; j < map.length; j++){
+                if (i == p.x && j == p.y) System.out.print('*' + " ");
+                else if(revealed[i][j]) System.out.print(map[i][j] + " ");
+                else System.out.print('X' + " ");
+            }
+            System.out.println();
+        }
     }
 
-    Point findStart(){
-        return p;
-    }
+    //Point findStart(){
+    //    return p;
+    //}
 
 
 }

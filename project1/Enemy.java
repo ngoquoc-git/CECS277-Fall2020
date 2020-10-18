19deed5e239ea1c7f0b56ea34cb6af7c31d04b5e
quocngo-git <ngoquoc.work@gmail.com>
2020-10-18 00:21:03 -0700

    Finish Enemy

--- project1/Enemy.java
+++ project1/Enemy.java
@@ -1,3 +1,36 @@
-public class Enemy{
-    
+import java.util.Random;
+public class Enemy extends Entity{
+    private Item item;
+
+    /**
+     * Default constructor
+     * @param n enemy's name
+     * @param mHP enemy's maximum health points
+     * @param i enemy's item
+     */
+    public Enemy(String n, int mHP, Item i){
+        super(n, mHP);
+        item = i;
+    }
+
+    /**
+     * return value of the item
+     * @return item's value
+     */
+    public Item getItem(){
+        return item;
+    }
+
+    /**
+     * Enemy attacks entity e
+     * @param e entity that will be attcked
+     * @return s String represents the attacking status 
+     */
+    @Override
+    public String attack(Entity e){
+        Random rand = new Random();
+        int damage = rand.nextInt(8);
+        if (damage == 0) return "Enemy hits you, but it is missed";
+        return getName() + " hits you and does " + damage + " damages.\n";
+    }
 }

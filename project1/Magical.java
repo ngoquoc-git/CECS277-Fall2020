b033ec7f69cb38ec3aa65b542ae8f591da712a90
quocngo-git <ngoquoc.work@gmail.com>
2020-10-18 17:40:12 -0700

    Finish Magical

--- project1/Magical.java
+++ project1/Magical.java
@@ -1,24 +1,25 @@
 public interface Magical {
+    
     public static final String MAGIC_MENU = "1. Magical Missile.\n2. Fire Ball.\n3. Thunderclap.\n";
 
     /**
      * Use magical missile on entity e
      * @param e Enity that will be attacked
-     * @return
+     * @return string status of the magical missile attack
      */
     public String magicalMissile(Entity e);
     
     /**
      * Use fire ball on entity e
      * @param e Enity that will be attacked
-     * @return
+     * @return string status of the fire ball attack
      */
     public String fireBall(Entity e);
 
     /**
      * Use thunder clap on entity e
      * @param e Enity that will be attacked
-     * @return
+     * @return string status of the thunder clap attack
      */
     public String thunderClap(Entity e);
 }

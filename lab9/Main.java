8496cf98bfc4fdf36bc52ea7283258f10775862d
quocngo-git <ngoquoc.work@gmail.com>
2020-10-27 18:57:16 -0700

    initiate the lab
    create main and words.txt

---
+++ lab9/Main.java
@@ -0,0 +1,33 @@
+public class Main{
+    public static void main(String[] args){
+
+    }
+
+    public static void readFile(){
+
+    }
+
+    public static void moveIter(){
+
+    }
+
+    public static void addWord(){
+
+    }
+
+    public static void removeWord(){
+
+    }
+
+    public static void printForward(){
+
+    }
+
+    public static void printReserve(){
+
+    }
+
+    public static void menu(){
+        
+    }
+}

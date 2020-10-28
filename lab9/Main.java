f30dbcd5aeadedebe189d54880efccaa405d802b
quocngo-git <ngoquoc.work@gmail.com>
2020-10-27 19:13:24 -0700

    Implement readFile

--- lab9/Main.java
+++ lab9/Main.java
@@ -1,10 +1,30 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
 
     }
 
-    public static void readFile(){
-
+    public static List<String> readFile(){
+        List<String> myList = new LinkedList<String>();
+        try{
+            File readMyFile = new File("words.txt");
+            Scanner sc = new Scanner(readMyFile);
+            while (sc.hasNextLine()){
+                String readInput = sc.nextLine();
+                myList.add(readInput);
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File was not found");
+            e.printStackTrace();
+        }
+        return myList;
     }
 
     public static void moveIter(){
@@ -28,6 +48,6 @@ public class Main{
     }
 
     public static void menu(){
-        
+
     }
 }

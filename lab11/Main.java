6b98e3a5db499ea34cd6e6263020d214252d779b
quocngo-git <ngoquoc.work@gmail.com>
2020-11-10 19:39:00 -0800

    finish main

--- lab11/Main.java
+++ lab11/Main.java
@@ -2,6 +2,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args){
         DoorFactory doors = new DoorFactory();
+        Random rand = new Random();
+        int level = 1;
+        int doorType = rand.nextInt(5) + 1;
 
+        while (level < 4){
+            System.out.println("You have reached level "+ level);
+            doors.openDoor(doorType);
+            level+=1;
+            doorType = rand.nextInt(5) + 1;
+        }
+
+        System.out.println("Congratulation, you have successfully finish the escape room.");
     }
 }

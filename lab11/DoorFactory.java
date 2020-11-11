9de6ca724e970cf96ff564ae58a7af87e5cd084b
quocngo-git <ngoquoc.work@gmail.com>
2020-11-10 19:25:19 -0800

    finish DoorFactory

--- lab11/DoorFactory.java
+++ lab11/DoorFactory.java
@@ -2,12 +2,16 @@ public class DoorFactory{
     public Door createDoor(int type){
         switch(type){
             case 1: return new BasicDoor();
-            case 2: return new Locke
+            case 2: return new LockedDoor();
+            case 3: return new DeadboltDoor();
+            case 4: return new ComboDoor();
+            case 5: return new CodeDoor();
+            default: return null;
         }
     }
 
     public void openDoor(int type){
-        Door dr = creatDoor(type);
+        Door dr = createDoor(type);
         dr.examine();
     }
 }

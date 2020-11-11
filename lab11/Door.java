8624b432d739efcb55b4b1ed7f16b596da664024
quocngo-git <ngoquoc.work@gmail.com>
2020-11-10 19:19:54 -0800

    create Door interface

---
+++ lab11/Door.java
@@ -0,0 +1,38 @@
+public interface Door {
+    /**
+     * Let users know what door is it
+     * @return door's name and description
+     */
+    public String examine();
+    
+    /**
+     * Available options for users
+     * @return a set of options
+     */
+    public String menu();
+
+    /**
+     * Check if the door is unlocked from given option
+     * @param option Users' option from menu
+     * @return User's action
+     */
+    public String unlock(int option);
+
+    /**
+     * Check if the door is open
+     * @return true if door is open, false otherwise
+     */
+    public boolean open();
+
+    /**
+     * Clue appears when users failed to open the door
+     * @return help message
+     */
+    public String clue();
+    
+    /**
+     * Success message if door is open
+     * @return success message
+     */
+    public String success();
+}

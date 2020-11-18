c9ab782a9d930f1104fb7e60a3b9448697461f4a
quocngo-git <ngoquoc.work@gmail.com>
2020-11-17 22:34:47 -0800

    finished main

--- lab12/Main.java
+++ lab12/Main.java
@@ -1,17 +1,42 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args){
         
-        Scanner sc = new Scanner(System.in);
+        Monster mon;
         int userInput;
 
         System.out.println("Monster Creator!");
+        System.out.println("Choose a base monster:");
+        System.out.println("1.Alien.\n2.Beast.\n3.Undead.");
+        userInput = CheckInput.getIntRange(1,3);
+
+        //Tried to use switch, but i caused error
+        if(userInput == 1) mon = new Alien();
+        else if(userInput == 2) mon = new Beast();
+        else mon = new Undead();
         
         
-        userInput = sc.nextInt();
+        System.out.println(mon.getName() + " has " + mon.getHP() + " HP, and attacks for " + mon.attack() + " damage.");
+        
+       
+        do{
+            System.out.println( "Add an ability:\n1. Fire.\n2. Ice\n3. Electro.\n4. Quit." );
 
-
-
+            userInput = CheckInput.getIntRange(1,4);
+            switch( userInput ){
+                case 1: mon = new Fire(mon);
+                System.out.println(mon.getName() + " has " + mon.getHP() + " HP, and attacks for " + mon.attack() + " damage.");
+                break;
+                case 2:
+                mon = new Ice(mon);
+                System.out.println(mon.getName() + " has " + mon.getHP() + " HP, and attacks for " + mon.attack() + " damage.");
+                break;
+                case 3:
+                mon = new Electro(mon);
+                System.out.println(mon.getName() + " has " + mon.getHP() + " HP, and attacks for " + mon.attack() + " damage.");
+                break;
+                case 4: break;
+            }
+        }while (userInput != 4);
+        System.out.println("Game Over.");
     }
 }

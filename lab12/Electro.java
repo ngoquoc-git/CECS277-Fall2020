56924ef7243d6282b8edae86abe29befa47e0fda
quocngo-git <ngoquoc.work@gmail.com>
2020-11-17 21:29:00 -0800

    Create Monster Abilities

---
+++ lab12/Electro.java
@@ -0,0 +1,14 @@
+public class Electro extends MonsterDecorator{
+
+    public Electro(Monster monster, String n, int h) {
+        super(monster, n, h);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int attack() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
---
+++ lab12/Ice.java
@@ -0,0 +1,14 @@
+public class Ice extends MonsterDecorator{
+
+    public Ice(Monster monster, String n, int h) {
+        super(monster, n, h);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int attack() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}

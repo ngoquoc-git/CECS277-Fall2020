dade4533e4bfbb0b11ad236e97d5f4c49a2546d7
quocngo-git <ngoquoc.work@gmail.com>
2020-11-17 21:29:11 -0800

    implement main

--- lab12/Main.java
+++ lab12/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         
+        Scanner sc = new Scanner(System.in);
+        int userInput;
+
+        System.out.println("Monster Creator!");
+        
+        
+        userInput = sc.nextInt();
+
+
+
     }
 }

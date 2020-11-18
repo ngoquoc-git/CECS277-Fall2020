6cbefb08ca8a2b83739bbe2537eb6a8ef7525e87
quocngo-git <ngoquoc.work@gmail.com>
2020-11-17 19:35:57 -0800

    finished Monster

--- lab12/Monster.java
+++ lab12/Monster.java
@@ -1,3 +1,20 @@
-abstract class Monster{
+public abstract class Monster{
+    private String name;
+    private int hp;
+
+    public Monster(String n, int h){
+        name = n;
+        hp = h;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public int getHP(){
+        return hp;
+    }
+
+    public abstract void attack();
 
 }

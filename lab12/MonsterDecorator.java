f58b19d9bb4aa66ba671e1f08419b1285cac0340
quocngo-git <ngoquoc.work@gmail.com>
2020-11-17 21:29:52 -0800

    Finish MonsterDecorator

--- lab12/MonsterDecorator.java
+++ lab12/MonsterDecorator.java
@@ -1,3 +1,14 @@
-public class MonsterDecorator {
+public abstract class MonsterDecorator extends Monster{
+
+    private Monster mon;
     
+    public MonsterDecorator(Monster monster, String n, int h) {
+        super(n, h);
+        mon = monster;
+    }
+    
+    @Override
+    public int attack() {
+        return mon.attack();
+    }
 }

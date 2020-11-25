71dae601e328fc1b5ba9870b6f65d9f5b834fad3
quocngo-git <ngoquoc.work@gmail.com>
2020-11-24 22:04:45 -0800

    javadoc Puppy

--- lab13/Puppy.java
+++ lab13/Puppy.java
@@ -1,39 +1,65 @@
 public class Puppy{
-    /** keeps track of each state */
+    /** keeps track of state status*/
     private PuppyState state;
+    /** Count eating times*/
     private int eatCount;
+    /** COunt playing times*/
     private int playCount;
     
+    /**
+     * Constructor that create a puppy
+     * set puppy in asleep state as default
+     * set eat and play to 0
+     */
     public Puppy() {
         state = new AsleepState(); 
         eatCount = 0;
         playCount = 0;
     }
     
+    /**
+     * Method to determine the next state of puppy
+     * @param s puppy's next state
+     */
     public void setState(PuppyState s) {state = s;}
 
-    public void feedPuppy(){
-        state.feed(this);
-    }
+    /**
+     * Feed puppy action that will be called in main
+     */
+    public void feedPuppy() {state.feed(this);}
 
-    public void playWithPuppy(){
-        state.play(this);
-    }
+    /**
+     * Play action that will be called in main
+     */
+    public void playWithPuppy() {state.play(this);}
 
-    public void incrementEatCount(){
-        eatCount++;
-    }
-    public void incrementPlayCount(){
-        playCount++;
-    }
+    /**
+     * Method to increase eat count by 1 
+     */
+    public void incrementEatCount() {eatCount++;}
+
+    /**
+     * Method to increase eat count by 1 
+     */
+    public void incrementPlayCount() {playCount++;}
+
+    /**
+     * Method that set bnoth eat count and play count to 0
+     */
     public void reset(){
         eatCount = 0;
         playCount = 0;
     }
-    public int getEatCount(){
-        return eatCount;
-    }
-    public int getPlayCount(){
-        return playCount;
-    }
+
+    /**
+     * Method taht retrives eat count
+     * @return eatCount to check if the puppy is full
+     */
+    public int getEatCount() {return eatCount;}
+
+    /**
+     * Method taht retrives play count
+     * @return playCount to check if the puppy is tired
+     */
+    public int getPlayCount() {return playCount;}
 }

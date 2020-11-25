9008241c1e6d95528d3e46e727f52498df57d767
quocngo-git <ngoquoc.work@gmail.com>
2020-11-24 21:57:03 -0800

    javadoc PuppyState

--- lab13/PuppyState.java
+++ lab13/PuppyState.java
@@ -1,4 +1,12 @@
 public interface PuppyState {
+    /**
+     * Action feed the puppy
+     * @param p puppy that will be fed
+     */
     public void feed(Puppy p);
+    /**
+     * Action feed the puppy
+     * @param p puppy that will be fed
+     */
     public void play(Puppy p);
 }

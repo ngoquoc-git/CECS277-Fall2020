f1fb344e5783a9b9ac6a7a6ad99e93e87b378eb7
quocngo-git <ngoquoc.work@gmail.com>
2020-11-24 21:30:37 -0800

    Finished Puppy

--- lab13/Puppy.java
+++ lab13/Puppy.java
@@ -1,3 +1,39 @@
 public class Puppy{
+    /** keeps track of each state */
+    private PuppyState state;
+    private int eatCount;
+    private int playCount;
     
+    public Puppy() {
+        state = new AsleepState(); 
+        eatCount = 0;
+        playCount = 0;
+    }
+    
+    public void setState(PuppyState s) {state = s;}
+
+    public void feedPuppy(){
+        state.feed(this);
+    }
+
+    public void playWithPuppy(){
+        state.play(this);
+    }
+
+    public void incrementEatCount(){
+        eatCount++;
+    }
+    public void incrementPlayCount(){
+        playCount++;
+    }
+    public void reset(){
+        eatCount = 0;
+        playCount = 0;
+    }
+    public int getEatCount(){
+        return eatCount;
+    }
+    public int getPlayCount(){
+        return playCount;
+    }
 }

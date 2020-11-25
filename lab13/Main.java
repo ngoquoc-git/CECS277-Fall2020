7c8c5d50ec5488131c1ce2a4ce4262ae5f823763
quocngo-git <ngoquoc.work@gmail.com>
2020-11-24 21:30:48 -0800

    Finished Main

--- lab13/Main.java
+++ lab13/Main.java
@@ -1,3 +1,18 @@
 public class Main {
-    
+    public static void main(String[] args){
+        int dec;
+        Puppy pup = new Puppy();
+
+        System.out.println("Congratulations on your new puppy!");
+        System.out.println("What would you like to do?\n1. Feed.\n2. Play.");
+        dec = CheckInput.getIntRange(1, 2);
+
+        do{
+            if(dec == 1) pup.feedPuppy();
+            else if (dec == 2) pup.playWithPuppy();
+            else System.out.println("Game Over.");
+            System.out.println("What would you like to do?\n1. Feed.\n2. Play.\n3. Quit.");
+            dec = CheckInput.getIntRange(1, 3);
+        }while(dec != 3);
+    }
 }

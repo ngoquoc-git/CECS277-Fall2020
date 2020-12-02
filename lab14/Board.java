c971ff25b760b55b473b619d37d2c64f4f466910
quocngo-git <ngoquoc.work@gmail.com>
2020-12-02 00:45:22 -0800

    javadoc

--- lab14/Board.java
+++ lab14/Board.java
@@ -1,15 +1,21 @@
+/**
+* this class creates a board for the game tic tac toe
+*/
 public class Board {
     /**
-     * 
+     * creates a board based on a 2D array for the game
      */
     private char[][] board = {{' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
 
     /**
-     * 
-     * @param token
-     * @param row
-     * @param col
-     * @return
+     * allows the player to choose where the token, x or o, will
+     * be placed on the array, Board
+     * @param token a character that is either x or o to be placed on 
+     * the game Board
+     * @param row the row number on the board to place the token
+     * @param col the column number on the board to place the token
+     * @return if the space is not taken on the board the value it true
+     * if the space is taken it returns false
      */
     public boolean placeToken(char token, int row, int col){
         boolean isPlaced = false;
@@ -21,7 +27,8 @@ public class Board {
     }
 
     /**
-     * 
+     * prints out the border, column and row number, for the
+     * tic tac toe game
      */
     public void display(){
         System.out.println(" 0  1  2");
@@ -37,24 +44,25 @@ public class Board {
     }
 
     /**
-     * 
-     * @return
+     * Saves the board in the current state with characters on it
+     * @return returns the current board with character on it
      */
     public Memento save(){
         return new Memento(board);
     }
 
     /**
-     * 
-     * @param object
+     * the state of the previous board is saved
+     * @param m the prevoius state of the board desired
      */
     public void restore(Memento m){
         board = m.getState();
     }
 
     /**
-     * 
-     * @return
+     * Check the winning conditions of the game
+     * Either win or draw if the board is filled and no winners
+     * @return character x for player 1 and o for player 2 or computer
      */
     public char winner(){
         boolean isFull = false;

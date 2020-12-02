92b381d8be372571c37c488acf17470124908aa8
quocngo-git <ngoquoc.work@gmail.com>
2020-12-02 00:49:11 -0800

    playerVplayer
    last commit messup
    undo

--- lab14/Main.java
+++ lab14/Main.java
@@ -1,9 +1,6 @@
 import java.util.Stack;
 
-/**
-* group 10 Lab 14
-* @author Rivera, Corrine and Quoc Ngo
-*/
+
 public class Main {
     public static void main(String[] args){
         Stack <Memento> history = new Stack <Memento>();
@@ -19,59 +16,59 @@ public class Main {
     }
 
     public static void playerVplayer(Board b, Stack<Memento> history) {
-        int inputs = 0;
-        int turns = 1;
-        do{    
-            if(turns%2 != 0){
-                
-                b.display();
-                System.out.println("1. place token");
-                System.out.println("2. revert");
-                System.out.println("3. quit");
-                inputs = CheckInput.getIntRange(1,3);
-              
-                    //Place a token
-                    if(inputs == 1) {
-                        int inputR;
-                        int inputC;
-                
-                        System.out.println("Place an x");
-                        System.out.print("Row: ");
-                        inputR = CheckInput.getIntRange(0,2);
-                        System.out.print("Col: ");
-                        inputC = CheckInput.getIntRange(0,2);
-                        
-                        if(b.placeToken('x', inputR, inputC)) {
-                            turns++;
-                            
-                        }
-                        else System.out.println("invalid location");
-                        
-                    }
-                    //Revert
-                    else if(inputs == 2) {
-                        if(!history.empty()) {
-                            b.restore(history.pop());
-                        }
-                        else System.out.println("nothing to revert");
-                    if(turns % 2 == 0) turns--;
-                    }
-                    //Quit
-                    else {
-                        System.out.println("Game Over");
-                        break;
-                    }
-                
-            }
-            else {
-                botEasy(b);
-                history.push(b.save());
-                turns++;
-            }
-        } while(b.winner() == ' ' && inputs != 3);
+        int input, turns = 1;
+        b.display();
+		System.out.println("1. place token");
+		System.out.println("2. revert");
+		System.out.println("3. quit");
+		input = CheckInput.getIntRange(1,3);
+		do {
+			if(input == 1) {
+				char token;
+				int inputR;
+				int inputC;
+				
+				if(turns%2 == 0) {
+					token = 'o';
+				}else token = 'x';
+				
+				System.out.println("Place an "+token);
+				System.out.print("Row: ");
+				inputR = CheckInput.getIntRange(0,2);
+				System.out.print("Col: ");
+				inputC = CheckInput.getIntRange(0,2);
 
-        if(b.winner() == 'x') System.out.println("Player wins.");
-        else if(b.winner() == 'o') System.out.println("Computer wins.");
+				if(b.placeToken(token, inputR, inputC)) {
+                    turns++;
+                    if(token == 'o') history.push(b.save());
+                }
+                else System.out.println("invalid location");
+				
+			}
+			
+			else if(input == 2) {
+				if(!history.empty()) {
+					b.restore(history.pop());
+                }
+                else System.out.println("nothing to revert");
+            if(turns % 2 == 0) turns--;
+			}
+			
+            else {
+                System.out.println("Game Over");
+                break;
+            }
+			
+			b.display();
+			System.out.println("1. place token");
+			System.out.println("2. revert");
+			System.out.println("3. quit");
+            input = CheckInput.getIntRange(1,3);
+            if(input == 3) System.out.println("Game Over");
+        } while(b.winner() == ' ' && input != 3);
+        
+        if(b.winner() == 'x') System.out.println("Player 1 wins.");
+        else if(b.winner() == 'o') System.out.println("Player 2 wins.");
         else System.out.println("The board is fully filled and there is no winner.");
     }
 

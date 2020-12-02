b97d9e8db2debaaece1ae48599dd8f9585838a25
quocngo-git <ngoquoc.work@gmail.com>
2020-12-02 00:45:34 -0800

    javadoc

--- lab14/Memento.java
+++ lab14/Memento.java
@@ -1,12 +1,16 @@
+/**
+* this class allows the board to revert back to its previous state
+*/
 public class Memento {
     /**
-     * 
+     * 3x3 char array to store the state of the board
      */
     private char[][] board = new char[3][3];
 
     /**
-     * 
-     * @param b
+     * places the charaters that were previously on the board
+     * and makes a copy of the array
+     * @param b passes a board to store
      */
     public Memento(char[][] b){
         for(int i = 0; i < b.length; i++){
@@ -17,8 +21,8 @@ public class Memento {
     }
 
     /**
-     * 
-     * @return
+     * shows the character array of the board
+     * @return board returns the char array
      */
     public char[][] getState(){
         return board;

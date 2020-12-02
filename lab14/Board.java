02090a84cc1b3163da2ac09227d452b9aefa7deb
quocngo-git <ngoquoc.work@gmail.com>
2020-12-01 21:25:23 -0800

    finsihed ec 2 winner function

--- lab14/Board.java
+++ lab14/Board.java
@@ -52,7 +52,53 @@ public class Board {
     }
 
     public char winner(){
-        
-        return 'a';
+        boolean isFull = false;
+        boolean isWin = false;
+        boolean breakLoop = false;
+        int i = 0, j = 0;
+        int rw, cl;
+        char winToken = ' ';
+
+        //Check if the board is fully placed with tokens
+        for (; i < board.length; i++){
+            for(; j < board[i].length; j++){
+                if (board[i][j] == ' ') break;
+            }
+            if (board[i][j] == ' ') {
+                breakLoop = true;
+                break;
+            }
+        }
+        if (breakLoop == false) isFull = true;
+
+        //Check win condition horizontally
+        for (rw = 0; rw < 3; rw++){
+            if(board[rw][0] == board[rw][1] && board[rw][0] == board[rw][2]){
+                isWin = true;
+                winToken = board[rw][0];
+                break;
+            }
+        }
+        //Check win condition vertically
+        for (cl = 0; cl < 3; cl++){
+            if(board[0][cl] == board[1][cl] && board[0][cl] == board[2][cl]){
+                isWin = true;
+                winToken = board[0][cl];
+                break;
+            }
+        }
+        //Check win condition diagonally
+        if(board[1][1] == board[0][0] && board[1][1] == board[2][2]){
+            isWin = true;
+            winToken = board[1][1];
+        }
+        if(board[1][1] == board[0][2] && board[1][1] == board[2][0]){
+            isWin = true;
+            winToken = board[1][1];
+        }
+
+        if(isWin == true) return winToken;
+        if(isFull == true) return 'c';
+        return ' ';
     }
 }

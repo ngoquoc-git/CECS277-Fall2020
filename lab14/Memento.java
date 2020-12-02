42a05781144f6bf0ae375436de7e2da0dc78b013
quocngo-git <ngoquoc.work@gmail.com>
2020-12-01 23:24:24 -0800

    minor fix

--- lab14/Memento.java
+++ lab14/Memento.java
@@ -11,7 +11,7 @@ public class Memento {
     public Memento(char[][] b){
         for(int i = 0; i < b.length; i++){
             for (int j = 0; j < b[i].length; j++){
-                this.board[i][j] = b [i][j];
+                this.board[i][j] = b[i][j];
             }
         }
     }

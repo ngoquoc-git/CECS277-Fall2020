e8611a5642e9b5d0638c0d999ff2d6a4906443de
quocngo-git <ngoquoc.work@gmail.com>
2020-12-08 15:04:41 -0800

    create function for Decorator

--- project2/WarlockDecorator.java
+++ project2/WarlockDecorator.java
@@ -1,3 +1,10 @@
-public class WarlockDecorator {
+public abstract class WarlockDecorator extends Enemy{
+    
+    private Enemy em;
+
+    public WarlockDecorator(Enemy enemy, String n, int mHP, Item i) {
+        super(n, mHP+1, i);
+        em = enemy;
+    }
     
 }
--- project2/WarriorDecorator.java
+++ project2/WarriorDecorator.java
@@ -1,3 +1,26 @@
-public class WarriorDecorator {
+/**
+ * 
+ */
+public abstract class WarriorDecorator extends Enemy{
+
+    /** */
+    private Enemy em;
+
+    /**
+     * 
+     * @param enemy
+     * @param n
+     * @param mHP
+     * @param i
+     */
+    public WarriorDecorator(Enemy enemy, String n, int mHP, Item i) {
+        super(n, mHP + 2, i);
+        em = enemy;
+    }
+
+    public String attack(Entity e){
+        //e.takeDamage(2);
+        return null;
+    }
     
 }
